31c2248ab5fe27ec1ebb233698e3d792dd4cc80a
Sofia <2003sasp@gmail.com>
2023-01-10 15:34:48 +0000

    idk

--- mbroker/mbroker.c
+++ mbroker/mbroker.c
@@ -1,9 +1,48 @@
 #include "logging.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/unistd.h>
+
+#define TAMMSG 1000
+
+
 
 int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
-    fprintf(stderr, "usage: mbroker <pipename>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+
+    int fcli, fserv;
+    ssize_t n;
+    char buf[TAMMSG];
+
+    unlink("/tmp/servidor");
+    unlink("/tmp/cliente");
+
+    if (mkfifo("/tmp/servidor", 0777) < 0) {
+        exit (1);
+    }
+    if (mkfifo("/tmp/cliente", 0777) < 0) {
+        exit (1);
+    }
+
+    if ((fserv = open("/tmp/servidor", O_RDONLY)) < 0) {
+	    exit(1);
+    }
+    if ((fcli = open("/tmp/cliente", O_WRONLY)) < 0) {
+        exit(1);
+    }
+	
+    while (1) {
+        n = read(fserv, buf, TAMMSG);
+        if (n <= 0) break;
+        n = write(fcli, buf, TAMMSG);
+    }
+
+    close(fserv);
+    close(fcli);
+    unlink("/tmp/servidor");
+    unlink("/tmp/cliente");
+    
+    return 0;
 }
