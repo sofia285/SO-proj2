ddf3bd6a517049db87e1412a6282736da6dbdc73
Sofia <2003sasp@gmail.com>
2023-01-11 16:30:21 +0000

    fifos

--- mbroker/mbroker.c
+++ mbroker/mbroker.c
@@ -1,38 +1,58 @@
-#include "logging.h"
+//#include "logging.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <sys/unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define TAMMSG 1000
 
+void server(char* register_name_pipe, char* max_sessions);
 
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
+    
+    printf("%s\n", argv[0]);
+            
+    if (argc != 3) {
+        exit(1);
+    }
+    server(argv[1], argv[2]);
+    
+    return 0;
+}
+
+void server(char* register_name_pipe, char* max_sessions) {
 
     int fcli, fserv;
     ssize_t n;
     char buf[TAMMSG];
 
-    unlink("/tmp/servidor");
+    printf("register_name_pipe: %s | max_sessions: %s\n", register_name_pipe, max_sessions);
+
+    unlink(register_name_pipe);
     unlink("/tmp/cliente");
 
-    if (mkfifo("/tmp/servidor", 0777) < 0) {
+    if (mkfifo(register_name_pipe, 0777) < 0) {
         exit (1);
     }
     if (mkfifo("/tmp/cliente", 0777) < 0) {
         exit (1);
     }
 
-    if ((fserv = open("/tmp/servidor", O_RDONLY)) < 0) {
-	    exit(1);
+    
+    if ((fserv = open(register_name_pipe, O_RDONLY)) < 0) {
+        exit(1);
     }
     if ((fcli = open("/tmp/cliente", O_WRONLY)) < 0) {
         exit(1);
     }
-	
+    
+    printf("Awaiting message\n");
+    fflush(stdout);
+
     while (1) {
         n = read(fserv, buf, TAMMSG);
         if (n <= 0) break;
@@ -41,8 +61,7 @@ int main(int argc, char **argv) {
 
     close(fserv);
     close(fcli);
-    unlink("/tmp/servidor");
+    unlink(register_name_pipe);
     unlink("/tmp/cliente");
-    
-    return 0;
+
 }
--- publisher/pub.c
+++ publisher/pub.c
@@ -1,4 +1,4 @@
-#include "logging.h"
+/*#include "logging.h"
 
 int main(int argc, char **argv) {
     (void)argc;
@@ -6,4 +6,52 @@ int main(int argc, char **argv) {
     fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
     WARN("unimplemented"); // TODO: implement
     return -1;
+}*/
+
+//#include "logging.h"
+
+#include <stdio.h> 
+#include <string.h> 
+#include <fcntl.h> 
+#include <sys/stat.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <stdlib.h>
+
+#define TAMMSG 1000
+
+void produzMsg(char *buf) {
+strcpy(buf, "Mensagem de teste");
 }
+
+void trataMsg(char *buf) {
+printf("Recebeu: %s\n", buf);
+}
+
+int main(int argc, char **argv) {
+  int fcli, fserv;
+  char buf[TAMMSG];
+  ssize_t n;
+
+  /* Os pipes são criados e destruídos pelo servidor */
+  /* Abrir os pipes */
+  if (argc < 1) {
+    exit(1);
+  }
+
+  if ((fserv = open (argv[1], O_WRONLY)) < 0)
+	exit(1);
+  if ((fcli = open ("/tmp/cliente", O_RDONLY)) < 0) {
+    exit(1);
+  }
+
+  produzMsg(buf);
+  n = write(fserv, buf, TAMMSG);
+  n = read(fcli, buf, TAMMSG);
+trataMsg(buf);
+
+  /* Fechar os pipes */
+  close(fserv);
+  close(fcli);
+}
+
