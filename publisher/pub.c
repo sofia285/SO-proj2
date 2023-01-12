c9a7f2e458ca2661b0f5e5e4ab75445ab0c18f6a
TomasTex <tomasteixeira5@gmail.com>
2023-01-12 14:38:07 +0000

    sofia fifos equivalent

--- publisher/pub.c
+++ publisher/pub.c
@@ -1,21 +1,53 @@
 #include "logging.h"
+#include <stdio.h> 
+#include <string.h> 
+#include <fcntl.h> 
+#include <sys/stat.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <stdlib.h>
 
-int main(int argc, char **argv) { 
-    if (argc != 4){ 
+#define TAMMSG 1000
+
+void produzMsg(char *buf) {
+    strcpy(buf, "Mensagem de teste");
+}
+
+void trataMsg(char *buf) {
+    printf("Recebeu: %s\n", buf);
+}
+
+int main(int argc, char **argv) {
+    int fcli, fserv;
+    char buf[TAMMSG];
+    ssize_t n; 
+    if (argc != 4){ //FIXME: SOFIA puseste <1 mas acho que é assim
         fprintf(stderr, "usage: pub <register_pipe_name> <pipe_name> <box_name>\n"); //DEBUG: pode n ser nada disto
-        return -1;
+        exit(1);
     }
     //pub <register_pipe_name> <pipe_name> <box_name>
     char const *pipenamePub = argv[1]; //argv[1] é o nome do pipe do pub
     char const *pipenameMBroker = argv[2]; //argv[2] é o nome do pipe do MBroker
     char const *box_name = argv[3]; //argv[3] é o nome da box
-    (void)pipenamePub;//TODO: remover isto
+    (void)pipenamePub;//TODO: remover isto tudo se n for preciso 
     (void)pipenameMBroker;
     (void)box_name;
+    (void)n;
 
+    if ((fserv = open (argv[1], O_WRONLY)) < 0){
+	    exit(1);
+    }
+    if ((fcli = open ("/tmp/cliente", O_RDONLY)) < 0) {
+        exit(1);
+    }
 
+    produzMsg(buf);
+    n = write(fserv, buf, TAMMSG);
+    n = read(fcli, buf, TAMMSG);
+    trataMsg(buf);
 
-    fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+    /* Fechar os pipes */
+    close(fserv);
+    close(fcli);
+    return 0;
 }
--- subscriber/sub.c
+++ subscriber/sub.c
@@ -1,11 +1,33 @@
 #include "logging.h"
+#include <stdio.h> 
+#include <string.h> 
+#include <fcntl.h> 
+#include <sys/stat.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <stdlib.h>
+
+#define TAMMSG 1000
+
+void produzMsg(char *buf) {
+    strcpy(buf, "Mensagem de teste");
+}
+
+void trataMsg(char *buf) {
+    printf("Recebeu: %s\n", buf);
+}
+
 
 int main(int argc, char **argv) { //sub <register_pipe_name> <pipe_name> <box_name>
+    int fcli, fserv;
+    char buf[TAMMSG];
+    ssize_t n;
+
     if (argc != 3){ 
         fprintf(stderr, "usage: sub <register_pipe_name> <box_name>\n"); //DEBUG: pode n ser nada disto
-        return -1;
+        exit(1);
     }
-    
+
     char const *pipenameSub = argv[1]; //argv[1] é o nome do pipe do SubpipenameSub
     char const *pipenameMBroker = argv[2]; //argv[2] é o nome do pipe do MBroker
     char const *box_name = argv[3]; //argv[3] é o nome da box
@@ -13,7 +35,20 @@ int main(int argc, char **argv) { //sub <register_pipe_name> <pipe_name> <box_na
     (void)pipenameMBroker;
     (void)box_name;
 
-    fprintf(stderr, "usage: sub <register_pipe_name> <box_name>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+    if ((fserv = open (argv[1], O_WRONLY)) < 0){
+	    exit(1);
+    }
+    if ((fcli = open ("/tmp/cliente", O_RDONLY)) < 0){
+        exit(1);
+    }
+
+    produzMsg(buf);
+    n = write(fserv, buf, TAMMSG);
+    n = read(fcli, buf, TAMMSG);
+    trataMsg(buf);
+
+    /* Fechar os pipes */
+    close(fserv);
+    close(fcli);
+    return 0;
 }
